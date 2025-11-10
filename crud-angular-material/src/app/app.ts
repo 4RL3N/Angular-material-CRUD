998b864b65b4dca83669653defe6851152b04e9c
4RL3N <arlen0filho04@gmail.com>
2025-11-09 22:45:43 -0300

    aplicacao finalizada e adicionado rodape

--- crud-angular-material/src/app/app.ts
+++ crud-angular-material/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon'
 import { MatAnchor } from "@angular/material/button";
+import { Rodape } from './rodape/rodape';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, MatAnchor],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, MatAnchor, Rodape],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
